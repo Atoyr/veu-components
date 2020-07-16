0744b91a1e1abfb53a6438ea0f65790188fa2f71
atoyr <lessironglance@gmail.com>
2020-07-16 19:47:19 +0900

    update vuejs config file add devtool

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const path = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/vue-components/' : '/'
+
 module.exports = {
-      publicPath: path,
-      outputDir: 'dist'
+  publicPath: path,
+  outputDir: 'dist',
+  configureWebpack: 
+  { 
+    devtool: 'source-map' 
+  }
 }
