7cb82ad150d375bd225d576a8fdacfdabccefdff
atoyr <lessironglance@gmail.com>
2020-07-22 18:39:57 +0900

    update add animation

--- src/components/DataBar.vue
+++ src/components/DataBar.vue
@@ -5,6 +5,9 @@
         <rect :fill="bgColor" :width="width" :height="height"></rect>
         <rect :fill="color" :width="barWidth" :height="height"></rect>
      </g>
+     <g>
+       <text :x="fontPosition.x" :y="fontPosition.y" text-anchor="end" stroke-width="1" :font-size="fontSize">{{visualValue}}</text>
+     </g>
     </svg>
   </div>
 </template>
@@ -40,12 +43,58 @@ export default {
     bgColor: {
       type: String,
       default: "#dddddd"
+    },
+    isDrumRoll: {
+      type: Boolean,
+      default: false
     }
   },
   computed:{
-    barWidth() {
-      return this.value / (Math.abs(this.minValue) + Math.abs(this.maxValue)) * this.width;
+    percent() {
+      return this.value / (Math.abs(this.minValue) + Math.abs(this.maxValue));
     },
+    visualPercent() {
+      return this.isAnimating ? this.speedEaseoutPercent : this.percent;
+    },
+    visualValue() {
+      return parseInt((this.isDrumRoll ? this.visualPercent : this.percent) * (Math.abs(this.minValue) + Math.abs(this.maxValue)));
+    },
+    barWidth() {
+      return this.visualPercent * this.width;
+    },
+    fontSize() {
+      return this.height * 0.75;
+    },
+    fontPosition() {
+      return {x: this.width - 4, y: this.height * 0.8 }
+    }
+  },
+  mounted() {
+    this.startAnimate();
+  },
+  methods: {
+    startAnimate: function() {
+      this.isAnimating = true;
+      this.animate();
+    },
+    animate: function() {
+      if (this.isAnimating) {
+        this.speedEaseoutPercent += (this.percent - this.speedEaseoutPercent) * 0.1; //フレーム更新毎に加算
+         
+        if(this.speedEaseoutPercent > this.percent - this.percent * 0.005){
+          this.isAnimating = false;
+        }else {
+          this.animation = requestAnimationFrame(this.animate);
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      animation: {},
+      isAnimating: false,
+      speedEaseoutPercent: 0,
+    }
   }
 }
 </script>
